7c7cd9473a44c0aed122352080223fb33b84d84f
Mayank Raj <lui.chang420@gmail.com>
2022-08-17 22:03:11 +0530

    update : added opposite Number

---
+++ DSA Questions/oppositeNumber.js
@@ -0,0 +1,13 @@
+// return the opposite number like input = 1 output= -1 input = -123 output = 123
+
+function opposite(n){
+    if(n){
+        return -n
+    }
+    return 0
+   
+}
+
+console.log(opposite(-12))
+console.log(opposite(0))
+console.log(opposite(11.345))
