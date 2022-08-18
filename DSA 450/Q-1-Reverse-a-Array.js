4960740792fabe384f263b267531dddb5f035c61
Mayank Raj <lui.chang420@gmail.com>
2022-08-18 11:43:53 +0530

    update : alternate logic added to reverse array

--- DSA 450/Q-1-Reverse-a-Array.js
+++ DSA 450/Q-1-Reverse-a-Array.js
@@ -3,6 +3,15 @@ function reverseArray(array){
     return array.reverse()
 }
 
+function reverseArray2(array){
+    let start=0;
+    let end=array.length-1;
+    while(start<end){
+        [array[start++],array[end--]]=[array[end],array[start]]
+    }
+    return array
+}
+
 console.log(reverseArray([1,2,3,4,5,6]))
-console.log(reverseArray(['1','2','3','4','5','6']))
-console.log(reverseArray(['1','2',3,'4','5','6']))
+console.log(reverseArray([1,2,3,4,5,6]))
+console.log(reverseArray2([1,2,3,4,5,6]))
